0b99a6f5aed5e7b4b000734e88693120261a1faa
vinod soba <vinod_soba@hotmail.co.uk>
2023-09-17 15:37:01 +0100

    created the footer and working on portfolio page

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,11 +1,21 @@
+import React, { useContext } from 'react'
+import  { Link }  from 'react-router-dom'
 import { NavigationWrapper } from "./Navigation.style";
+import { ThemeContext } from './../../Theme';
+
 
 
 function Navigation() {
+
+    const { theme }  = useContext(ThemeContext);
+  
     return ( 
         <NavigationWrapper>
-              <ul>
-                <li>Home</li>
+            <ul>
+                <Link to="/services" style={theme === 'dark-theme' ? { color: '#fff' } : { color: '#000'}}>Services</Link>
+                <Link to="/work">Work</Link>
+                <Link to="/about">About</Link>
+                <Link to="/contact">Contact</Link>
             </ul>
         </NavigationWrapper>
      );
---
+++ src/pages/Work.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+// axios to query the API
+import axios from 'axios';
+
+// module
+import Portfolio from '../components/portfolio/Portfolio';
+
+const URL = "http://localhost/vinodsoba/wp-json/wp/v2/pages?limit=1";
+
+class Work extends Component {
+    state = {  
+        data: [],
+        isLoading: false
+    } 
+
+    componentDidMount() {
+        axios.get(URL)
+        .then(response => this.setState({
+            data: response.data,
+            isLoading: true
+        }))
+        .catch(err => console.log(err))
+    }
+    render() { 
+        const { data, isLoading } = this.state;
+        console.log(data);
+        return (
+            <>
+                 <div className='header-hero-container'>
+                    { data.map(item => item.id === 61 ? 
+                        <h1 key={item.id}>{item.title.rendered}</h1>  
+                    : null  
+                    )}
+                </div>
+                <Portfolio />
+            </>
+           
+            
+        );
+    }
+}
+ 
+export default Work;
