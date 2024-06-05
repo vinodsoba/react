3344750bed51a081c7b83d442b919d78acb46877
Vinod Soba <Vinod.Soba@Addisonlee.com>
2024-06-05 21:56:18 +0100

    removed the hooks from footerlink1, home and services

--- src/components/footerlink1/FooterLink1.jsx
+++ src/components/footerlink1/FooterLink1.jsx
@@ -1,29 +1,7 @@
-import React, { useContext } from 'react'
-import { Link } from 'react-router-dom';
-import useFetchMenu from '../../hooks/use-fetch-menu';
+import React from 'react'
 
-import { ThemeContext } from '../../Theme';
-
-import './style.css';
-
-function FooterLink1() {
-    const { data } = useFetchMenu([]);
-    const { theme } = useContext(ThemeContext);
-
-    console.log(data);
-
+export default function FooterLink1() {
   return (
-    <div className="footer-links">
-        {
-            data.map(item =>  
-            <ul>
-                <li key={item.term_id}><Link style={theme === 'light-theme' ? { color: 'var(--text-primary-dark)'} : { color: 'var(--text-primary)'}} to={item.url}>{item.title}</Link></li>
-            </ul>
-               
-            )
-        }
-    </div>
+    <div>FooterLink1</div>
   )
 }
-
-export default FooterLink1
