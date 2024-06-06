39e0737f3919229e5a6d27b34d8b6887c55be21f
Vinod Soba <Vinod.Soba@Addisonlee.com>
2024-06-06 16:50:47 +0100

    added hooks to the footer for testing that links show in footer

--- src/components/footermenu/FooterLinks.jsx
+++ src/components/footermenu/FooterLinks.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Link } from 'react-router-dom';
+import useFetchMenu from '../../hooks/use-fetch-menu';
 
-export default function FooterLinks() {
+import { ThemeContext } from '../../Theme';
+
+import './style.css';
+
+function FooterLink1() {
+    const { data } = useFetchMenu([]);
+    const { theme } = useContext(ThemeContext);
+
+    console.log(data);
+
   return (
-    <div>FooterLinks</div>
+    <div className="footer-links">
+        {
+            data.map(item =>  
+            <ul>
+                <li key={item.term_id}><Link style={theme === 'light-theme' ? { color: 'var(--text-primary)'} : { color: 'var(--text-primary-light)'}} to={item.url}>{item.title}</Link></li>
+            </ul>
+               
+            )
+        }
+    </div>
   )
 }
+
+export default FooterLink1
