44c6ae244c805c455475c48c1e912fac0b859571
Vinod Soba <Vinod.Soba@Addisonlee.com>
2024-06-12 12:53:59 +0100

    all api now working and reverted component files

--- src/hooks/use-fetch-menu.jsx
+++ src/hooks/use-fetch-menu.jsx
@@ -26,7 +26,7 @@ function useFetchMenu() {
     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
 
                     },
-                        body: JSON.stringify(data)
+                        body: JSON.parse(data)
                     })
                     
                 setData(response);    
