261839ff2dafb2c1d6dae40d6fb37a5f4930caad
Vinod Soba <Vinod.Soba@Addisonlee.com>
2024-08-05 17:00:51 +0100

    amended fetch data to fullurl inc domain name

--- src/hooks/use-fetch-data.jsx
+++ src/hooks/use-fetch-data.jsx
@@ -16,7 +16,7 @@ function useFetchData() {
             setLoading(true);
             try {
                 const {data: response } = await axios.get(
-                    "/wp-json/wp/v2/pages?per_page=15",
+                    "https://www.vinodsoba.co.uk/wp-json/wp/v2/pages?per_page=15",
                     {
                         headers: {
                             Authorization: "Basic " + base64.encode(username + ':' + password),
